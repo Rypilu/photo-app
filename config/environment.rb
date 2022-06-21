a94eeb039e4dce53f3cccf1b82bc710de5f6146e
rypilu <rpiluski@gmail.com>
2022-06-21 06:56:15 -0400

    setup email functionality for sign-up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# smtp
+# config.action_mailer.delivery_method = :smtp
+# host = "https://obscure-badlands-83420.herokuapp.com"
+# config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
+  :domain => 'gmail.com',
+  :enable_starttls_auto => true
+}
